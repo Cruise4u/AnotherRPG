4e22079474106ea952b3e7d00b856cb8d9fd643e
Cruise4u <pedrofrancisco90@gmail.com>
2021-08-01 01:43:08 +0200

    Commit before changing graphics

---
+++ Assets/Resources/Logic/Scripts/VFX/VFXAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class VFXAnimation : MonoBehaviour
+{
+    public GameObject[] vfxObjects;
+
+    public void CallNextVFXObject(int index)
+    {
+        vfxObjects[index].SetActive(true);
+        vfxObjects[index].GetComponent<Animator>().SetTrigger("vfxTrigger");
+    }
+
+
+
+}
