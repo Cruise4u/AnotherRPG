63f9c40f0c87ea41b3c972f13f117230c52301f2
Cruise4u <pedrofrancisco90@gmail.com>
2021-09-02 21:05:28 +0200

    Commit before changing computer

---
+++ Assets/Resources/Logic/Scripts/VFX/DivineShield.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class DivineShield : VFXBase
+{
+    public GameObject starVfx;
+    public GameObject bubbleVfx;
+    public void SetInitialScale(GameObject vfxObject)
+    {
+        vfxObject.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+    }
+    public void ScaleVfxSprite(GameObject vfxObject, float threshold)
+    {
+        if(vfxObject.transform.localScale.magnitude < threshold)
+        {
+            vfxObject.transform.localScale += new Vector3(Time.deltaTime, Time.deltaTime, Time.deltaTime);
+        }
+    }
+    public override void ClearVFX(GameObject vfxObject)
+    {
+        vfxObject.SetActive(false);
+    }
+    public override IEnumerator TriggerVfxRoutine(float time, bool isAnglePositive)
+    {
+        SetInitialScale(starVfx);
+        SetAlphaCut(starVfx, 0.7f);
+        yield return new WaitForSeconds(time);
+        ClearVFX(starVfx);
+    }
+    public IEnumerator StartBubbleRoutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+        bubbleVfx.SetActive(true);
+        yield return new WaitForSeconds(time);
+    }
+    public void Update()
+    {
+        ScaleVfxSprite(starVfx,1.0f);
+        ChangeAlphaCutValue(starVfx);
+        //if(bubbleVfx.activeSelf == true)
+        //{
+        //    ScaleVfxSprite(bubbleVfx,0.5f);
+        //}
+    }
+}
--- Assets/Resources/Logic/Scripts/VFX/VFXBase.cs
+++ Assets/Resources/Logic/Scripts/VFX/VFXBase.cs
@@ -5,5 +5,14 @@ using UnityEngine;
 public abstract class VFXBase : MonoBehaviour
 {
     public abstract IEnumerator TriggerVfxRoutine(float time,bool isAnglePositive);
-    public abstract void ClearVFX();
+    public abstract void ClearVFX(GameObject vfxObject);
+    public virtual void SetAlphaCut(GameObject vfxObject, float maxValue)
+    {
+        vfxObject.GetComponent<SpriteMask>().enabled = true;
+        vfxObject.GetComponent<SpriteMask>().alphaCutoff = maxValue;
+    }
+    public virtual void ChangeAlphaCutValue(GameObject vfxObject)
+    {
+        vfxObject.GetComponent<SpriteMask>().alphaCutoff -= Time.deltaTime;
+    }
 }
